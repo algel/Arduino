c358a27b5114df21ea0e10e15b4ec7c41ed0bc81
Alexey Pozdnyakov <algel.fam@gmail.com>
2017-11-07 08:45:47 +0300

    Remove all old ode from Arduino.Monitor and use Stylet mvvm framework

--- Solid.Arduino.Monitor/App.xaml.cs
+++ Solid.Arduino.Monitor/App.xaml.cs
@@ -5,13 +5,22 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Stylet.Xaml;
 
 namespace Solid.Arduino.Monitor
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        /// <summary>Raises the <see cref="E:System.Windows.Application.Startup" /> event.</summary>
+        /// <param name="e">A <see cref="T:System.Windows.StartupEventArgs" /> that contains the event data.</param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Resources.Add(typeof(ApplicationLoader), new ApplicationLoader { Bootstrapper = new Bootstrapper() });
+
+            base.OnStartup(e);
+        }
     }
 }
